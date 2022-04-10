620e9d603cdf0c14d14e3566db4353ae4137987b
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2022-04-10 17:03:46 +0530

    apollo skillwallet setup

---
+++ lib/apollo.ts
@@ -0,0 +1,12 @@
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql
+  } from "@apollo/client";
+
+  const client = new ApolloClient({
+    uri: 'https://dev-api.skillwallet.id/api/',
+    cache: new InMemoryCache()
+  });
